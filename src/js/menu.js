1234b48e2f2fd12821641fd78e5eab58b7d9195e
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-07-31 13:31:24 +0300

    js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.menu.classList.toggle('is-open');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
