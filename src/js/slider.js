92a559f79e5b66b21e2698f13e33365579386f22
pavloit <dba.devop@gmail.com>
2023-08-04 19:06:15 +0300

    fix review slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,33 +1,8 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import {Navigation} from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-new Swiper('.comment-swiper', {
-  modules: [Pagination],
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  grabCursor: true,
-  spaceBetween: 25,
-
-  breakpoints: {
-    100: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1280: {
-      slidesPerView: 3,
-      spaceBetween: 32,
-    },
-  },
-});
 
 new Swiper('.yahts-cards', {
   modules: [Navigation],
