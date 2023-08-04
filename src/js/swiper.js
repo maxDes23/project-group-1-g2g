66cd7bc40fbc7499f8117423cb7486a0948bb792
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-08-04 15:49:52 +0300

    оі

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,55 +1,21 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-new Swiper('.yahts-cards', {
-  modules: [Pagination],
+  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
   },
 
-  grabCursor: true,
-  spaceBetween: 25,
-
-  breakpoints: {
-    100: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1280: {
-      slidesPerView: 3,
-      spaceBetween: 32,
-    },
-  },
-});
-
-new Swiper('.yahts-cards', {
-  modules: [Navigation],
+  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next, .under-button-next',
-    prevEl: '.swiper-button-prev, .under-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 
-  spaceBetween: 16,
-
-  breakpoints: {
-    100: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
-    768: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-    1280: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
-    },
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
   },
 });
